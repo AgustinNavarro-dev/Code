9b99a92207fb2cf752c85b0c5dbf285dd391c4d9
Agustin Navarro <77839366+AgustinNavarro-dev@users.noreply.github.com>
2022-08-09 13:56:38 +0200

    Add files via upload

---
+++ files.py
@@ -0,0 +1,4 @@
+#con file() podemos abrir un file. no abrirlo, pero si acceder a el
+#con open() lo abrimos "handle = open(filename, mode)" filename: diario.txt
+#mode: con "r" leemos, y con "w" escribimos
+#con /n vamos a otra linea, se usa adentro de strings "Hola /n mundo"
