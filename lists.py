5fa90d95fbdf3064737defb634c0dfb1e48222f5
Agustin Navarro <navarroagustin1993@gmail.com>
2022-10-08 11:17:58 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+##buscar el avrga con una lista
+
 numlist = list()
 while True:
     inp = input("Enter a number: ")
@@ -7,3 +9,8 @@ while True:
 average = sum(numlist) / len(numlist)
 
 print(average)
+
+## no se usa este metodo para grandes listas de numeros, ya que podrian
+## llevar mucho espacio en memoria
+## para 100 millones de numeros, por ej, se utilizaria una avrg normal
+## se sumarian dos variables siempre y luego se dividirian.
